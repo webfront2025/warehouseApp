ac627296a3768070a0c0acbbc482fb7f5c4477e4
Mansoureh <laleh.safarian@gmail.com>
2026-03-20 12:56:12 +0100

    12:56

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -1,14 +1,15 @@
-
-
+from pydantic import BaseModel
 from fastapi import APIRouter, HTTPException
 
 router = APIRouter()
 
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
 @router.post("/login")
-def login(user: dict):
-
-    if user["username"] == "admin" and user["password"] == "1234":
-
+def login(login_data: LoginRequest):
+    if login_data.username == "admin" and login_data.password == "1234":
         return {"token": "abc123"}
-
+    
     raise HTTPException(status_code=401, detail="Invalid login")
